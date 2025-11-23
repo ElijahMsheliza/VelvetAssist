1580d430bf869f5b76ec3e46b11a1b449f8e6ba1
VelvetAssist User <user@velvetassist.com>
2025-11-23 14:11:38 -0500

    Config: Prepare for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/VelvetAssist" : "",
+  basePath: isGithubActions ? "/VelvetAssist" : "",
   images: {
     unoptimized: true,
   },
